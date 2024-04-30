9f558a984b88fea1d5ec4b13cb36294916f10411
Kevin <kevindana99@gmail.com>
2024-04-29 22:53:28 -0300

    update server

--- server/integrations/NuevaEnergia/modules/CategoriesMenu/index.js
+++ server/integrations/NuevaEnergia/modules/CategoriesMenu/index.js
@@ -1,45 +1,47 @@
 import {icons} from './components/icons';
 
 const addedIconsToMenu = () => {
-    const menu = document.querySelectorAll(".o_shop_collapse_category")[0]
-      .children[0];
-    const menuMobile = document.querySelectorAll(".o_shop_collapse_category")[1]
+  const categories = document.querySelectorAll(".o_shop_collapse_category");
+  const menu = document.querySelectorAll(".o_shop_collapse_category")[0]
     .children[0];
-    const mapMenu = menu.children;
-  const mapMenuMobile = menuMobile.children;
-  const mapMenus = [mapMenu,mapMenuMobile];  
-  mapMenus.forEach((menuParam) => {
-    Array.from(menuParam).forEach((e, index) => {
-      if (index < 1) {
-        null;
-      } else {
-        const temp = document.createElement("div");
-        if (icons[index -1]){
-        temp.innerHTML = icons[index - 1];
-        const firstElement = e.children[0].firstChild;
-        if (!e.children[0].classList.contains("accordion-header")) {
-          e.children[0].style.setProperty(
-            "justify-content",
-            "flex-start",
-            "important"
-          );
-          e.children[0].style.setProperty("flex-wrap", "nowrap", "important");
-          e.children[0].style.setProperty(
-            "align-items",
-            "flex-start",
-            "important"
-          );
-        }
-        e.children[0].insertBefore(temp, firstElement);
-        e.style.display = "block";
-      }else{
-        e.style.display = "none";
+  const menuMobile = document.querySelectorAll(".o_shop_collapse_category")[1]
+  .children[0];
+  const mapMenu = menu.children;
+const mapMenuMobile = menuMobile.children;
+
+  const mapMenus = [mapMenu,mapMenuMobile];
+
+mapMenus.forEach((menuParam) => {
+  Array.from(menuParam).forEach((e, index) => {
+    if (index < 1) {
+      null;
+    } else {
+      const temp = document.createElement("div");
+      if (icons[index -1]){
+      temp.innerHTML = icons[index - 1];
+      const firstElement = e.children[0].firstChild;
+      if (!e.children[0].classList.contains("accordion-header")) {
+        e.children[0].style.setProperty(
+          "justify-content",
+          "flex-start",
+          "important"
+        );
+        e.children[0].style.setProperty("flex-wrap", "nowrap", "important");
+        e.children[0].style.setProperty(
+          "align-items",
+          "flex-start",
+          "important"
+        );
       }
-      
-      }
-    });
-  })
-  
+      e.children[0].insertBefore(temp, firstElement);
+    
+      categories.forEach((e) => {
+e.style.display = "block"
+      })
+    }
+    }
+  });
+})
   };
 
  export default {
